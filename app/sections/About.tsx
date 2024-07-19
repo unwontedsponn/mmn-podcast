7132bd37713e55dc4cf8e980be9f8a2c1a82bffa
unwontedsponn <benpaulspooner@gmail.com>
2024-07-19 11:39:59 +0100

    Updated About to tsx

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -1,11 +1,15 @@
-"use client"
+"use client";
 import FadeIn from "../utilities/FadeIn";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 
-export default function About ( {setAboutInView} ) {
+interface AboutProps {
+  setAboutInView: (inView: boolean) => void;
+}
+
+export default function About({ setAboutInView }: AboutProps) {
   const { ref, inView } = useInView({
-    threshold: 0.5,  // Trigger when 50% of the element is in view
+    threshold: 0.5, // Trigger when 50% of the element is in view
   });
 
   useEffect(() => {
@@ -14,7 +18,6 @@ export default function About ( {setAboutInView} ) {
 
   return (
     <FadeIn className="flex min-h-screen">
-      
       {/* SVG/Image container */}
       <div className="hidden lg:block w-1/2" ref={ref} id="about">
         <img
@@ -28,10 +31,13 @@ export default function About ( {setAboutInView} ) {
       <div className="lg:w-1/2 flex flex-col justify-center p-12 space-y-6 lg:pr-40">
         <h1 className="text-5xl text-black font-bold">What is MMN?</h1>
         <p className="text-xl body-poppins text-black">
-          {`Welcome to the Music Maker Network’s podcast. Where we chat to the people who make music happen. From artists to producers, engineers to composers, if you are looking to learn more about the ‘behind the scenes’ of music, you’ve come to the right place. `}
+          {`Welcome to the Music Maker Network’s podcast. Where we chat to the people who make music happen. From artists to producers, engineers to composers, if you are looking to learn more about the ‘behind the scenes’ of music, you’ve come to the right place.`}
         </p>
-        <button href="/watch-video" className="text-center border-2 border-red text-white bg-red lg:text-red lg:bg-snow hover:bg-red hover:text-white text-xl py-2 px-4 w-40 text-left">Watch video <div className="text-xs">COMING SOON</div></button>
+        <button className="text-center border-2 border-red text-white bg-red lg:text-red lg:bg-snow hover:bg-red hover:text-white text-xl py-2 px-4 w-40 text-left">
+          Watch video
+          <div className="text-xs">COMING SOON</div>
+        </button>
       </div>
     </FadeIn>
   );
-};
+}
